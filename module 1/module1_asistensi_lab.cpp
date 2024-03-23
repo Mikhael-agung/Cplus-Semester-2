83ebdf0dfe6480a931f0cfbbadc224ff5f1f3d5b
mikhaelagung <mikhaelagung3@gmail.com>
2024-03-23 23:04:23 +0700

    update tugas Asistensi

--- module 1/module1_asistensi_lab.cpp
+++ module 1/module1_asistensi_lab.cpp
@@ -4,27 +4,53 @@
 
 using namespace std;
 
-struct detailBarang
-{
+struct detailBarang {
     int harga;
     int stock;
 };
 
-struct Barang
-{
+struct Barang {
     string nama;
     detailBarang detail;
 };
 
 vector<Barang> dataBarang;
 
-void tambahBarang () {
+void tambahBarang() {
+    Barang barangBaru;
 
+    cout << "Masukan nama barang yang ingin di tambahkan: " << endl;
+    cin >> barangBaru.nama;
+    cout << "Masukan Jumlah Barang: " << endl;
+    cin >> barangBaru.detail.stock;
+    cout << "Masukan Harga Barang: " << endl;
+    cin >> barangBaru.detail.harga;
+
+    dataBarang.push_back(barangBaru);
+
+    if (!dataBarang.empty())
+    {
+        cout << "barang berhasil di tambahkan!!" << endl;
+    }
+    else
+    {
+        cout << "Barang gagal di tambahkan" << endl;
+    }
 }
 
-    int
-    main()
-{
+void viewDataBarang(){
+    if (dataBarang.empty())
+    {
+        cout << "Barang masih kosong!!" << endl;
+        return;
+    }
 
+    cout << "===================" << endl;
+    
+}
+
+int main()
+{
+    tambahBarang();
     return 0;
 }

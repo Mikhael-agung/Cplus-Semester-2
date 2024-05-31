e22ee937e23465fc98322efd8f99f74415bac872
mikhaelagung <mikhaelagung3@gmail.com>
2024-06-01 00:53:00 +0700

    puszh

--- Latihan_S2/Discord.cpp
+++ Latihan_S2/Discord.cpp
@@ -152,6 +152,11 @@ void tampilkanDataMahasiswa() {
     }
 }
 
+/**
+ * @brief The main function of the program.
+ * 
+ * @return int The exit status of the program.
+ */
 int main() {
     string nama, jurusan;
     int umur;
--- module 3/praktikum_07651.cpp
+++ module 3/praktikum_07651.cpp
@@ -1,82 +1,66 @@
 #include <iostream>
-#include <stack>
 #include <iomanip>
-#include <vector>
 #include <string>
 using namespace std;
 
-struct Barang_07651
-{
-    int jumlah_07651;
+struct Barang_07651 {
     string nama_07651;
+    Barang_07651* next;
 };
 
-void viewBarang_07651(stack<Barang_07651> s)
-{
-    if (s.empty())
-    {
+struct Stack_07651 {
+    Barang_07651* top;
+};
+
+void initStack(Stack_07651& barang) {
+    barang.top = nullptr;
+}
+
+void push(Stack_07651& barang, const string& nama) {
+    Barang_07651* newBarang = new Barang_07651;
+    newBarang->nama_07651 = nama;
+    newBarang->next = barang.top;
+    barang.top = newBarang;
+    cout << "Barang berhasil ditambahkan." << endl;
+}
+
+void viewBarang(const Stack_07651& barang) {
+    if (!barang.top) {
         cout << "Gudang Barang kosong." << endl;
         return;
     }
 
-    vector<Barang_07651> temp;
-    while (!s.empty())
-    {
-        temp.push_back(s.top());
-        s.pop();
-    }
-
     cout << "List Barang Yang ada di gudang." << endl;
     cout << "No. || Nama Barang ||" << endl;
-    for (int i = temp.size() - 1; i >= 0; i--)
-    {
-        cout << (temp.size() - i) << setw(15) << temp[i].nama_07651 << endl;
-        s.push(temp[i]);
+
+    Barang_07651* current = barang.top;
+    int count = 1;
+    while (current) {
+        cout << count << setw(15) << current->nama_07651 << endl;
+        current = current->next;
+        count++;
     }
 }
 
-void ambilBarang(stack<Barang_07651>& Gudang) {
-    if (!Gudang.empty()) {
-        string namaBarang;
-    
-        cin.ignore(); 
-        cout << "Masukkan nama barang yang akan diambil: ";
-        getline(cin, namaBarang);
-
-        bool found = false;
-        stack<Barang_07651> temp;
-
-        while (!Gudang.empty()) {
-            Barang_07651 spec = Gudang.top();
-            if (spec.nama_07651 == namaBarang) {
-                cout << "Barang " << spec.nama_07651 << " berhasil diambil." << endl;
-                found = true;
-            } else {
-                temp.push(spec);
-            }
-            Gudang.pop();
-        }
-
-        while (!temp.empty()) {
-            Gudang.push(temp.top());
-            temp.pop();
-        }
-
-        if (!found) {
-            cout << "Barang dengan nama " << namaBarang << " tidak ditemukan dalam gudang." << endl;
-        }
-    } else {
+void ambilBarang(Stack_07651& barang) {
+    if (!barang.top) {
         cout << "Gudang kosong. Tidak ada barang yang bisa diambil." << endl;
+        return;
     }
-}
 
-int main()
-{
-    stack<Barang_07651> Gudang;
+    Barang_07651* temp = barang.top;
+    barang.top = barang.top->next;
+    cout << "Barang " << temp->nama_07651 << " berhasil diambil." << endl;
+    delete temp;
+
+};
+
+int main() {
+    Stack_07651 Gudang;
+    initStack(Gudang);
 
     int pilihan;
-    do
-    {
+    do {
         cout << "Pengelolahan Barang gudang PT. Gudang Gula" << endl;
         cout << "1. Tambah Barang" << endl;
         cout << "2. Tampilkan Nama Barang" << endl;
@@ -85,21 +69,18 @@ int main()
         cout << "Masukan Pilihan: ";
         cin >> pilihan;
 
-        switch (pilihan)
-        {
-        case 1:
-        {
-            Barang_07651 spec;
+        switch (pilihan) {
+        case 1: {
+            string nama;
             cin.ignore();
             cout << "Masukan Nama Barang: ";
-            getline(cin, spec.nama_07651);
-            Gudang.push(spec);
-            cout << "Barang berhasil ditambahkan." << endl;
+            getline(cin, nama);
+            push(Gudang, nama);
             break;
         }
 
         case 2:
-            viewBarang_07651(Gudang);
+            viewBarang(Gudang);
             break;
 
         case 3:
@@ -107,10 +88,11 @@ int main()
             break;
 
         case 4:
-            cout << "Program selesai";
+            cout << "Program selesai" << endl;
             break;
+
         default:
-            cout << "Pilihan Yang anda pilih tidak ada.";
+            cout << "Pilihan Yang anda pilih tidak ada." << endl;
             break;
         }
     } while (pilihan != 4);
